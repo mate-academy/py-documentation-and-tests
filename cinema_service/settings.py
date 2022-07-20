7bd4acef602138c980bc205c93524e19e23e949a
Yaroslav Havryliv <ghavryliv@gmail.com>
2022-07-20 17:22:35 +0300

    deleted CreateTokenView and AuthTokenSerializer

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "rest_framework.authtoken",
     "drf_spectacular",
     "debug_toolbar",
     "cinema",
