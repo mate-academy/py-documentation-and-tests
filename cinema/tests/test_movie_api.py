1eac8b13fdb9de4e84875fe4e27b91c0f02499e3
Roman <romanschneider9@gmail.com>
2022-07-24 23:01:48 +0300

    imports fix

--- cinema/tests/test_movie_api.py
+++ cinema/tests/test_movie_api.py
@@ -10,8 +10,11 @@ from rest_framework.test import APIClient
 from rest_framework import status
 
 from cinema.models import Movie, MovieSession, CinemaHall, Genre, Actor
-from cinema.serializers import MovieSerializer, MovieListSerializer, \
-    MovieDetailSerializer
+from cinema.serializers import (
+    MovieSerializer,
+    MovieListSerializer,
+    MovieDetailSerializer,
+)
 
 MOVIE_URL = reverse("cinema:movie-list")
 MOVIE_SESSION_URL = reverse("cinema:moviesession-list")
