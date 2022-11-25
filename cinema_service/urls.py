736cc95628a311440230c9a82873d911f51d3ee1
Liudmyla Kulinchenko <liudmilakulinchenko@gmail.com>
2022-11-25 01:11:41 +0100

    Move token endpoints from global urls to user/urls.py

--- cinema_service/urls.py
+++ cinema_service/urls.py
@@ -7,23 +7,12 @@ from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularRedocView,
 )
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView
-)
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/cinema/", include("cinema.urls", namespace="cinema")),
     path("api/user/", include("user.urls", namespace="user")),
-
-    path(
-        "api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"
-    ),
-    path(
-        "api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"
-    ),
-
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "api/doc/swagger/",
