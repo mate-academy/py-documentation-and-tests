32769074ffa93309362ba32f22b8d896bb902759
stanislavbublinsky <bublinsky.s.m@gmail.com>
2022-11-26 11:20:57 +0200

    deleted unused imports

--- user/serializers.py
+++ user/serializers.py
@@ -1,6 +1,5 @@
-from django.contrib.auth import get_user_model, authenticate
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
-from django.utils.translation import gettext as _
 
 
 class UserSerializer(serializers.ModelSerializer):
