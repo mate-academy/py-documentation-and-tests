0cad1e46f001ca89d8f858e81307285ba09b5f57
Roman Kolmakov <cerne313@gmail.com>
2022-11-28 00:35:20 +0200

    Fix settings.py

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -142,7 +142,15 @@ REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-    )
+    ),
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+        "anon": "10/min",
+        "user": "30/min",
+    }
 }
 
 SPECTACULAR_SETTINGS = {
