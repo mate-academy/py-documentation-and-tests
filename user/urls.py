96dc7ab45ce44d4a08303a9fa370d8edbef2f941
mltdll <ilya.vyplavin@gmail.com>
2022-11-29 09:26:46 +0200

    Fix user urls and detail view

--- user/urls.py
+++ user/urls.py
@@ -11,10 +11,10 @@ app_name = "user"
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
     path(
-        "api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"
+        "token/", TokenObtainPairView.as_view(), name="token_obtain_pair"
     ),
     path(
-        "api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"
+        "token/refresh/", TokenRefreshView.as_view(), name="token_refresh"
     ),
     path("me/", ManageUserView.as_view(), name="manage"),
 ]
