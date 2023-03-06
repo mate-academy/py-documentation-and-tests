231837343151628e7915a83b0b6e58115a27840e
Denys Tereshchuk <terrya@ukr.net>
2023-03-06 20:17:11 +0400

    Added default throttle classes and rates in settings.py

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -152,6 +152,14 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication",
     ],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '10/day',
+        'user': '30/day'
+    }
 }
 
 SPECTACULAR_SETTINGS = {
