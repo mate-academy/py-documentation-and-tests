d3a0dfc0788f4d70a670561eb718322e5c8cd778
Zhukov_Oleksandr <zhukov9523@gmail.com>
2023-04-12 14:42:57 +0300

    Added throttling

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -140,6 +140,11 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {"anon": "10/minute", "user": "30/minute"},
 }
 
 SIMPLE_JWT = {
