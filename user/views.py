163914d19ad25d5a89375010d20a875c6723ec0e
Andriy Demeshko <ande777complaint@gmail.com>
2023-04-23 12:05:01 +0300

    Remove redundant imports

--- user/views.py
+++ user/views.py
@@ -1,11 +1,10 @@
 from drf_spectacular.utils import extend_schema
 from rest_framework import generics
-from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.settings import api_settings
 
-from user.serializers import UserSerializer, AuthTokenSerializer
+from rest_framework.permissions import IsAuthenticated
+
+
+from user.serializers import UserSerializer
 
 
 @extend_schema(tags=["user"])
