ee0eefa9acbaab3749fc371a92dc9591472d43bd
Arsen Makovei <makoveyarsen@gmail.com>
2023-04-24 13:38:42 +0300

    Updated type for OpenApiParameter: date

--- cinema/views.py
+++ cinema/views.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 from django.db.models import F, Count
+from drf_spectacular.types import OpenApiTypes
 from drf_spectacular.utils import extend_schema, OpenApiParameter
 from rest_framework import viewsets, mixins, status
 from rest_framework.decorators import action
@@ -188,8 +189,9 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
         parameters=[
             OpenApiParameter(
                 "date",
-                type=str,
-                description="Filter by date (ex. ?date=2024-10-08)"
+                type=OpenApiTypes.DATE,
+                location=OpenApiParameter.QUERY,
+                description="Filter by date (ex. ?date=2024-10-08)",
             ),
             OpenApiParameter(
                 "movie",
