861f95c4826a50450613a707af653fdbfb34a135
Andrii Skryl <a.skryl92@gmail.com>
2023-06-05 16:38:49 +0200

    Solution 3

--- cinema/views.py
+++ cinema/views.py
@@ -129,7 +129,7 @@ class MovieViewSet(
         parameters=[
             OpenApiParameter(
                 "title",
-                type={"type": "string", "items": {"type": "string"}},
+                type={"type": "string"},
                 description="Filter by title (ex. ?title=name)",
                 required=False,
             ),
