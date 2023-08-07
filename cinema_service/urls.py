d3a7c3e47afed7afa7e515abb41b96bed30e3dd9
Alim Selemetov <alimselemetow@gmail.com>
2023-08-07 22:46:50 +0300

    solution

--- cinema_service/urls.py
+++ cinema_service/urls.py
@@ -2,7 +2,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -11,6 +15,14 @@ urlpatterns = [
     path("__debug__/", include("debug_toolbar.urls")),
 
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
-    path("api/doc/swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
-    path("api/doc/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+    path(
+        "api/doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui"
+    ),
+    path(
+        "api/doc/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc"
+    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
