ab677424d37bacc69019fcfded321200512c2cf0
Illia Smyrnov <ilushasm@gmail.com>
2023-08-11 18:38:32 +0300

    Solution

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,23 @@
 from django.urls import path
-from user.views import CreateUserView, CreateTokenView, ManageUserView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
+
+from user.views import CreateUserView, ManageUserView
 
 app_name = "user"
 
 urlpatterns = [
+    path("login/", TokenObtainPairView.as_view(), name="login"),
+    path(
+        "api/token/refresh/",
+        TokenRefreshView.as_view(),
+        name="token_refresh"
+    ),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+
     path("register/", CreateUserView.as_view(), name="create"),
-    path("login/", CreateTokenView.as_view(), name="login"),
     path("me/", ManageUserView.as_view(), name="manage"),
 ]
