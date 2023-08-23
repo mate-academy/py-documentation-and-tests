f2d5864ae64a1458e40d36d0f3e568423f848d8c
Alex_Freud <NikiforenkoAS@i.ua>
2023-08-23 13:32:26 +0300

    Solution

--- user/serializers.py
+++ user/serializers.py
@@ -42,7 +42,8 @@ class AuthTokenSerializer(serializers.Serializer):
                 if not user.is_active:
                     msg = _("User account is disabled.")
                     raise serializers.ValidationError(
-                        msg, code="authorization"
+                        msg,
+                        code="authorization"
                     )
             else:
                 msg = _("Unable to log in with provided credentials.")
