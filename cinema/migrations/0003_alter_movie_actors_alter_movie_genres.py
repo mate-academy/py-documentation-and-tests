612cc319280c35304a0b79efed45e973b5037265
Nataliia Podoroha <ingenuous999@gmail.com>
2023-09-11 16:05:18 +0200

    Solution_with_black

--- cinema/migrations/0003_alter_movie_actors_alter_movie_genres.py
+++ cinema/migrations/0003_alter_movie_actors_alter_movie_genres.py
@@ -4,20 +4,19 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('cinema', '0002_initial'),
+        ("cinema", "0002_initial"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='movie',
-            name='actors',
-            field=models.ManyToManyField(blank=True, to='cinema.actor'),
+            model_name="movie",
+            name="actors",
+            field=models.ManyToManyField(blank=True, to="cinema.actor"),
         ),
         migrations.AlterField(
-            model_name='movie',
-            name='genres',
-            field=models.ManyToManyField(blank=True, to='cinema.genre'),
+            model_name="movie",
+            name="genres",
+            field=models.ManyToManyField(blank=True, to="cinema.genre"),
         ),
     ]
