40e699b2b474a2c4753a32c4783c1076a434dc77
Taras Savchyn <savik1992@gmail.com>
2023-09-20 00:00:30 +0300

    add filtering to movie-session doc

--- cinema/views.py
+++ cinema/views.py
@@ -10,7 +10,6 @@ from rest_framework.decorators import action
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
-from rest_framework.schemas import openapi
 from rest_framework.viewsets import GenericViewSet, ReadOnlyModelViewSet
 
 from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession, Order
@@ -187,6 +186,29 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
             queryset = queryset.filter(movie_id=int(movie_id_str))
 
         return queryset
+
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name="show_time",
+                type=OpenApiTypes.DATE,
+                location=OpenApiParameter.QUERY,
+                description="filtering by date",
+            ),
+
+            OpenApiParameter(
+                "movie",
+                type=OpenApiTypes.ANY,
+                description="filtering by movies"
+            ),
+         ]
+    )
+
+
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
 
     def get_serializer_class(self):
         if self.action == "list":
