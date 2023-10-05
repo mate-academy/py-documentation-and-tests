5a1151e0d2edab2d3d62584bb88933351ee6ce1b
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-10-05 22:14:19 +0300

    Add coma

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework.authtoken",
-    "drf_spectacular"
+    "drf_spectacular",
     "debug_toolbar",
     "cinema",
     "user",
