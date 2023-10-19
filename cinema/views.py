88ae16d623545fac9c0f1e2a9a3e583b31a1eb3a
Oleh Chernous <chernous.oleh@gmail.com>
2023-10-19 19:32:06 +0200

    Fix 'upload_image' custom action

--- cinema/views.py
+++ cinema/views.py
@@ -116,12 +116,10 @@ class MovieViewSet(
         """Endpoint for uploading image to specific movie"""
         movie = self.get_object()
         serializer = self.get_serializer(movie, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
 
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     @extend_schema(
         parameters=[
