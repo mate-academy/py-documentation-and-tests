bb446499d5baf509c3c442be609299e8617570c5
Pavlo Harbuz <lgsilay1991@gmail.com>
2023-10-23 17:29:53 +0300

    add token/verify/ to user/urls.py

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
-    TokenRefreshView
+    TokenRefreshView, TokenVerifyView
 )
 
 from user.views import CreateUserView, ManageUserView
@@ -12,5 +12,6 @@ urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
     path("me/", ManageUserView.as_view(), name="manage"),
 ]
