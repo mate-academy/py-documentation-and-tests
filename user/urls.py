3d50f78bee5b95e314e6ebb2198e2785b7501d15
moroz_olexandr <oleksandrmoroz2003@gmail.com>
2023-10-30 19:02:35 +0200

    add jwt token urls

--- user/urls.py
+++ user/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 from user.views import CreateUserView, ManageUserView
 
 app_name = "user"
@@ -6,4 +8,6 @@ app_name = "user"
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
     path("me/", ManageUserView.as_view(), name="manage"),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_obtain_pair"),
 ]
