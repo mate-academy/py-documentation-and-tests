b0f5d0a68a57ddaa08cc225683e875b78f2f91f5
Roman Burluka <103953904+romkapomka12@users.noreply.github.com>
2023-11-27 11:41:30 +0200

    Created test for MovieViewSet

--- cinema/tests/test_movie_api.py
+++ cinema/tests/test_movie_api.py
@@ -10,6 +10,7 @@ from rest_framework.test import APIClient
 from rest_framework import status
 
 from cinema.models import Movie, MovieSession, CinemaHall, Genre, Actor
+from cinema.serializers import MovieSerializer, MovieListSerializer, MovieDetailSerializer
 
 MOVIE_URL = reverse("cinema:movie-list")
 MOVIE_SESSION_URL = reverse("cinema:moviesession-list")
@@ -43,9 +44,7 @@ def sample_actor(**params):
 
 
 def sample_movie_session(**params):
-    cinema_hall = CinemaHall.objects.create(
-        name="Blue", rows=20, seats_in_row=20
-    )
+    cinema_hall = CinemaHall.objects.create(name="Blue", rows=20, seats_in_row=20)
 
     defaults = {
         "show_time": "2022-06-02 14:00:00",
@@ -157,3 +156,138 @@ class MovieImageUploadTests(TestCase):
         res = self.client.get(MOVIE_SESSION_URL)
 
         self.assertIn("movie_image", res.data[0].keys())
+
+
+class UnauthenticatedMovieApiTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_auth_required(self):
+        res = self.client.get(MOVIE_URL)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class AuthenticatedMovieApiTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            "test@myproject.com",
+            "password",
+        )
+        self.client.force_authenticate(self.user)
+
+        self.movie1 = sample_movie(title="Test Movie 1")
+        self.movie2 = sample_movie(title="Test Movie 2")
+        self.movie3 = sample_movie(title="Special Movie")
+
+        self.genre1 = sample_genre()
+        self.genre2 = sample_genre(name="TestGenre")
+        self.movie1.genres.add(self.genre1)
+        self.movie2.genres.add(self.genre2)
+
+        self.actor1 = sample_actor()
+        self.actor2 = sample_actor(first_name="Test", last_name="User")
+        self.movie1.actors.add(self.actor1)
+        self.movie2.actors.add(self.actor2)
+
+        self.serializer1 = MovieListSerializer(self.movie1)
+        self.serializer2 = MovieListSerializer(self.movie2)
+        self.serializer3 = MovieListSerializer(self.movie3)
+
+    def test_list_movies(self):
+        response = self.client.get(MOVIE_URL)
+
+        movies = Movie.objects.all()
+        serializer = MovieListSerializer(movies, many=True)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serializer.data)
+
+    def test_filter_movie_by_title(self):
+        response = self.client.get(MOVIE_URL, {"title": "Test Movie"})
+
+        self.assertIn(self.serializer1.data, response.data)
+        self.assertIn(self.serializer2.data, response.data)
+        self.assertNotIn(self.serializer3.data, response.data)
+
+    def test_filter_movie_by_genre(self):
+        response = self.client.get(MOVIE_URL, {"genres": f"{self.genre1.id},{self.genre2.id}"})
+
+        self.assertIn(self.serializer1.data, response.data)
+        self.assertIn(self.serializer2.data, response.data)
+        self.assertNotIn(self.serializer3.data, response.data)
+
+    def test_filter_movies_by_actor(self):
+        res = self.client.get(MOVIE_URL, {"actors": f"{self.actor1.id},{self.actor2.id}"})
+
+        self.assertIn(self.serializer1.data, res.data)
+        self.assertIn(self.serializer2.data, res.data)
+        self.assertNotIn(self.serializer3.data, res.data)
+
+    def test_retrieve_movie_detail(self):
+        url = detail_url(self.movie1.id)
+        response = self.client.get(url)
+
+        serializer = MovieDetailSerializer(self.movie1)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serializer.data)
+
+    def test_create_movie_forbidden(self):
+        payload = {
+            "title": "The Departed",
+            "description": "A man learns something extraordinary about himself after a devastating accident.",
+            "duration": 102
+        }
+
+        response = self.client.post(MOVIE_URL, payload)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class AdminMovieApiTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.admin_user = get_user_model().objects.create_user(
+            email="admin@admin.com",
+            password="1qazxcde3",
+            is_staff=True
+        )
+        self.client.force_authenticate(self.admin_user)
+
+        self.movie1 = sample_movie(title="Test Movie 1")
+
+        self.genre1 = sample_genre()
+        self.genre2 = sample_genre(name="TestGenre")
+        self.movie1.genres.add(self.genre1)
+
+        self.actor1 = sample_actor()
+        self.actor2 = sample_actor(first_name="Test", last_name="User")
+        self.movie1.actors.add(self.actor1)
+
+        self.serializer1 = MovieListSerializer(self.movie1)
+
+    def test_create_movie(self):
+        main_data = {
+            "title": "The Departed",
+            "description": "A man learns something extraordinary about himself after a devastating accident.",
+            "duration": 102,
+            "genres": [self.genre1.pk, self.genre2.pk],
+            "actors": [self.actor1.pk, self.actor2.pk]
+        }
+
+        response = self.client.post(MOVIE_URL, main_data)
+        movie = Movie.objects.get(id=response.data["id"])
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        for key in main_data:
+            if key in ("genres", "actors"):
+                continue
+            self.assertEqual(main_data[key], getattr(movie, key))
+
+    def test_delete_movie_not_allowed(self):
+        movie = sample_movie()
+        url = detail_url(movie.id)
+        response = self.client.delete(url)
+
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
--- cinema/views.py
+++ cinema/views.py
@@ -1,14 +1,12 @@
 from datetime import datetime
-
 from django.db.models import F, Count
+from drf_spectacular.utils import extend_schema, OpenApiParameter
 from rest_framework import viewsets, mixins, status
-from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import action
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
-from rest_framework.viewsets import GenericViewSet, ReadOnlyModelViewSet
-
+from rest_framework.viewsets import GenericViewSet
 from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession, Order
 from cinema.permissions import IsAdminOrIfAuthenticatedReadOnly
 
@@ -35,7 +33,6 @@ class GenreViewSet(
 ):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
-    authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
 
@@ -46,7 +43,6 @@ class ActorViewSet(
 ):
     queryset = Actor.objects.all()
     serializer_class = ActorSerializer
-    authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
 
@@ -57,7 +53,6 @@ class CinemaHallViewSet(
 ):
     queryset = CinemaHall.objects.all()
     serializer_class = CinemaHallSerializer
-    authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
 
@@ -69,7 +64,6 @@ class MovieViewSet(
 ):
     queryset = Movie.objects.prefetch_related("genres", "actors")
     serializer_class = MovieSerializer
-    authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
     @staticmethod
@@ -127,20 +121,42 @@ class MovieViewSet(
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name="title",
+                type={"type": "list", "items": {"type": "number"}},
+                description="filtering by title for movie",
+            ),
+            OpenApiParameter(
+                name="genres",
+                type={"type": "list", "items": {"type": "number"}},
+                description="filtering by genres for movie",
+            ),
+            OpenApiParameter(
+                name="actor",
+                type={"type": "list", "items": {"type": "number"}},
+                description="filtering by actor for movie",
+            ),
+        ]
+    )
+    def list(self, request, *args, **kwargs):
+        # your non-standard behaviour
+        return super().list(request, *args, **kwargs)
+
 
 class MovieSessionViewSet(viewsets.ModelViewSet):
     queryset = (
         MovieSession.objects.all()
         .select_related("movie", "cinema_hall")
         .annotate(
-            tickets_available=(
-                F("cinema_hall__rows") * F("cinema_hall__seats_in_row")
-                - Count("tickets")
-            )
+            tickets_available=F(
+                "cinema_hall__rows") * F(
+                "cinema_hall__seats_in_row") - Count(
+                "tickets")
         )
     )
     serializer_class = MovieSessionSerializer
-    authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
     def get_queryset(self):
@@ -167,6 +183,25 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
 
         return MovieSessionSerializer
 
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name="date",
+                type={"type": "list", "items": {"type": "number"}},
+                description="filtering by date for movie session",
+            ),
+            OpenApiParameter(
+                name="movie",
+                type={"type": "list", "items": {"type": "number"}},
+                description="filtering by movie for movie session",
+            ),
+        ]
+    )
+    # Only for docimentation purposes
+    def list(self, request, *args, **kwargs):
+        # your non-standard behaviour
+        return super().list(request, *args, **kwargs)
+
 
 class OrderPagination(PageNumberPagination):
     page_size = 10
@@ -183,7 +218,6 @@ class OrderViewSet(
     )
     serializer_class = OrderSerializer
     pagination_class = OrderPagination
-    authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
