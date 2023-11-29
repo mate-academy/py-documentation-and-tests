d40339d956d7840cc2b61c7eea837ac9544dbfbe
Mykola <nickkozlov90@gmail.com>
2023-11-29 18:56:08 +0200

    Add throttling settings

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -143,7 +143,15 @@ MEDIA_ROOT = BASE_DIR / "media"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
-    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle"
+        ],
+    "DEFAULT_THROTTLE_RATES": {
+        "anon": "10/minute",
+        "user": "30/minute"
+    }
 }
 
 SPECTACULAR_SETTINGS = {
@@ -151,5 +159,4 @@ SPECTACULAR_SETTINGS = {
     'DESCRIPTION': 'Order cinema tickets',
     'VERSION': '1.0.0',
     'SERVE_INCLUDE_SCHEMA': False,
-    # OTHER SETTINGS
 }
