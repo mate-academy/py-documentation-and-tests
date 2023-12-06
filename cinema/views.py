d0175488a2f0900471eaab838bcf1cf3b3780148
Kitaiskyi Oleksii <leshafamilenko@gmail.com>
2023-12-06 16:25:40 +0200

    delete unused imports

--- cinema/views.py
+++ cinema/views.py
@@ -2,7 +2,6 @@ from datetime import datetime
 
 from django.db.models import F, Count
 from rest_framework import viewsets, mixins, status
-from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import action
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
