fc461b5b95f6456db4f8c0b217496bf4aea9f0f4
Dzhura Rostislav <dzhurarostislav@gmail.com>
2023-12-21 13:57:54 +0200

    Solution

--- cinema/tests/test_movie_api.py
+++ cinema/tests/test_movie_api.py
@@ -6,13 +6,133 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
-from rest_framework.test import APIClient
+from rest_framework.test import APIClient, APITestCase
 from rest_framework import status
 
 from cinema.models import Movie, MovieSession, CinemaHall, Genre, Actor
+from cinema.serializers import MovieListSerializer, MovieDetailSerializer
 
 MOVIE_URL = reverse("cinema:movie-list")
 MOVIE_SESSION_URL = reverse("cinema:moviesession-list")
+
+
+class MovieViewSetTests(APITestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_list_movies_unauthorized(self):
+        response = self.client.get(MOVIE_URL)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class MovieViewSetAuthorizedTests(APITestCase):
+    def setUp(self):
+        self.auth_user = get_user_model().objects.create_user(
+            email="authuser@auth.com", password="authpass"
+        )
+        self.client.force_authenticate(self.auth_user)
+
+        genre_drama = Genre.objects.create(name="Drama")
+        genre_action = Genre.objects.create(name="Action")
+        actor_john_doe = Actor.objects.create(first_name="John", last_name="Doe")
+        actor_jane_doe = Actor.objects.create(first_name="Jane", last_name="Doe")
+
+        self.movie1 = Movie.objects.create(
+            title="Test Drama Movie", description="Drama movie", duration=120
+        )
+        self.movie1.genres.set([genre_drama])
+        self.movie1.actors.set([actor_john_doe])
+
+        self.movie2 = Movie.objects.create(
+            title="Test Action Movie", description="Action movie", duration=150
+        )
+        self.movie2.genres.set([genre_action])
+        self.movie2.actors.set([actor_jane_doe])
+
+        self.movie_url = reverse("cinema:movie-detail", args=[self.movie1.id])
+
+    def test_list_movies_by_title(self):
+        response = self.client.get(reverse("cinema:movie-list"), {"title": "Drama"})
+
+        serializer1 = MovieListSerializer(self.movie1)
+        serializer2 = MovieListSerializer(self.movie2)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+        self.assertIn(serializer1.data, response.data)
+        self.assertNotIn(serializer2.data, response.data)
+
+    def test_list_movies_by_genres(self):
+        response = self.client.get(
+            reverse("cinema:movie-list"), {"genres": f"{self.movie1.genres.first().id}"}
+        )
+
+        serializer1 = MovieListSerializer(self.movie1)
+        serializer2 = MovieListSerializer(self.movie2)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+        self.assertIn(serializer1.data, response.data)
+        self.assertNotIn(serializer2.data, response.data)
+
+    def test_list_movies_by_actors(self):
+        response = self.client.get(
+            reverse("cinema:movie-list"), {"actors": f"{self.movie1.actors.first().id}"}
+        )
+
+        serializer1 = MovieListSerializer(self.movie1)
+        serializer2 = MovieListSerializer(self.movie2)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+        self.assertIn(serializer1.data, response.data)
+        self.assertNotIn(serializer2.data, response.data)
+
+    def test_retrieve_movie_authorized(self):
+        response = self.client.get(self.movie_url)
+
+        serializer = MovieDetailSerializer(self.movie1)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(serializer.data, response.data)
+
+
+class MovieViewSetAdminTests(APITestCase):
+    def setUp(self):
+        self.admin_user = get_user_model().objects.create_user(
+            email="adminuser@admin.com", password="adminpass", is_staff=True
+        )
+        self.client.force_authenticate(self.admin_user)
+
+        self.movie = Movie.objects.create(title="Test Movie", duration=120)
+        self.movie_url = reverse("cinema:movie-detail", args=[self.movie.id])
+
+    def test_list_movies_admin(self):
+        response = self.client.get(reverse("cinema:movie-list"))
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_retrieve_movie_admin(self):
+        response = self.client.get(self.movie_url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_movie_admin(self):
+        genre_action = Genre.objects.create(name="Action")
+        actor_john_doe = Actor.objects.create(
+            first_name="John", last_name="Doe"
+        )
+        payload = {
+            "title": "New Test Movie",
+            "description": "Description for the new movie",
+            "duration": 120,
+            "genres": [genre_action.id],
+            "actors": [actor_john_doe.id]
+        }
+
+        response = self.client.post(reverse("cinema:movie-list"), payload)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
 
 def sample_movie(**params):
@@ -43,9 +163,7 @@ def sample_actor(**params):
 
 
 def sample_movie_session(**params):
-    cinema_hall = CinemaHall.objects.create(
-        name="Blue", rows=20, seats_in_row=20
-    )
+    cinema_hall = CinemaHall.objects.create(name="Blue", rows=20, seats_in_row=20)
 
     defaults = {
         "show_time": "2022-06-02 14:00:00",
