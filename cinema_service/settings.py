df5285140b0d1af178fba15570ea183b3adc3f20
nsdNite <nsd7night@gmail.com>
2024-01-17 10:26:35 +0200

    Add drf_spectacular to installed apps *facepalm*

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "drf_spectacular",
     "rest_framework",
     "debug_toolbar",
     "cinema",
