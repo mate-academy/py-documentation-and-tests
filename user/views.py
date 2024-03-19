303cdcb3110cc5db38678ae0ffdb1b4c8c0dcc33
MaxymChyncha <max.chyncha@gmail.com>
2024-03-19 13:59:05 +0200

    refactor(cinema/user): remove unnecessary authentication classes

--- user/views.py
+++ user/views.py
@@ -11,14 +11,8 @@ class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
 
 
-class CreateTokenView(ObtainAuthToken):
-    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
-    serializer_class = AuthTokenSerializer
-
-
 class ManageUserView(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
-    authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
 
     def get_object(self):
