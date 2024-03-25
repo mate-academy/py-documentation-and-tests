5e5d378e68a68a29dd3c926fedb34c959968e67e
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-03-25 12:21:30 +0200

    Add default throttling settings

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -139,6 +139,14 @@ MEDIA_ROOT = BASE_DIR / "media"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle"
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+        "anon": "10/min",
+        "user": "30/min"
+    },
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
