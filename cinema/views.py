049366d23d7968c48156d056dc80554fefd0e33c
Nataliia Korzh <clarioruberia@gmail.com>
2024-03-26 09:56:27 +0200

    Solution

--- cinema/views.py
+++ cinema/views.py
@@ -3,7 +3,6 @@ from datetime import datetime
 from drf_spectacular.utils import extend_schema, OpenApiParameter
 from django.db.models import F, Count
 from rest_framework import viewsets, mixins, status
-from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import action
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
