8c1cd5b252a0e40e5c915bac987a59837a41139d
Polonnikova Anna <xxxuko@gmail.com>
2024-03-26 09:43:33 +0200

    Solution

--- user/views.py
+++ user/views.py
@@ -1,23 +1,16 @@
 from rest_framework import generics
-from rest_framework.authentication import TokenAuthentication
-from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.settings import api_settings
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-from user.serializers import UserSerializer, AuthTokenSerializer
+from user.serializers import UserSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
 
 
-class CreateTokenView(ObtainAuthToken):
-    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
-    serializer_class = AuthTokenSerializer
-
-
 class ManageUserView(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
-    authentication_classes = (TokenAuthentication,)
+    authentication_classes = (JWTAuthentication,)
 
     def get_object(self):
         return self.request.user
