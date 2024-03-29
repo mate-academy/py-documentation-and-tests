c43f3e6db483cfa790bf3bd5cac1d685e52b6453
Sikach Illia <sikatch.ilja@gmail.com>
2024-03-29 18:26:57 +0200

    Delete redundant view and path

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
 
 from user.views import CreateUserView, CreateTokenView, ManageUserView
 
@@ -10,7 +10,6 @@ urlpatterns = [
     path("login/", CreateTokenView.as_view(), name="login"),
     path("me/", ManageUserView.as_view(), name="manage"),
     path("__debug__/", include("debug_toolbar.urls")),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
