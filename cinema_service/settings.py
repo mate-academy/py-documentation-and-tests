e72105daae596c76f2adcdfe8bbd7afcce9ae128
Molchanov Ihor <=>
2024-04-07 23:50:52 +0300

    add throttle rates

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -103,18 +103,26 @@ MEDIA_ROOT = BASE_DIR / "media"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
-    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+        "anon": "10/minute",
+        "user": "30/minute",
+    }
 }
 
 SPECTACULAR_SETTINGS = {
-    'TITLE': 'Cinema Movie API',
-    'DESCRIPTION': 'Order movie ticket for watching movie',
-    'VERSION': '1.0.0',
-    'SERVE_INCLUDE_SCHEMA': False,
-    'SWAGGER_UI_SETTINGS': {
-        'deepLinking': True,
-        'defaultModelRendering': "model",
-        'defaultModelsExpandDepth': 2,
-        'defaultModelExpandDepth': 2,
+    "TITLE": "Cinema Movie API",
+    "DESCRIPTION": "Order movie ticket for watching movie",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": False,
+    "SWAGGER_UI_SETTINGS": {
+        "deepLinking": True,
+        "defaultModelRendering": "model",
+        "defaultModelsExpandDepth": 2,
+        "defaultModelExpandDepth": 2,
     }
 }
