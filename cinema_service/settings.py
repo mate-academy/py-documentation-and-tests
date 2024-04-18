b1c2da580332d16537c3a177e5a3b64214011a46
Vladyslav Shepilov <shepsterya@gmail.com>
2024-04-18 14:22:26 +0300

    add throttling for authorized and unauthorized users

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -145,5 +145,13 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle"
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+        "anon": "10/min",
+        "user": "30/min"
+    }
 }
 
