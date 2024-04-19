38396741da003499fd69db5b222ed642c1ec80c0
Vladyslav Shepilov <shepsterya@gmail.com>
2024-04-19 21:06:40 +0300

    refactor tests

--- cinema/tests/test_movie_api.py
+++ cinema/tests/test_movie_api.py
@@ -168,18 +168,16 @@ class MovieViewSetTestCase(APITestCase):
         self.client.force_authenticate(user=self.user)
 
     def test_create_movie(self):
-        url = reverse("cinema:movie-list")
         data = {"title": "New Movie", "duration": 120}
-        response = self.client.post(url, data)
+        response = self.client.post(MOVIE_URL, data)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_list_movies(self):
-        url = reverse("cinema:movie-list")
-        response = self.client.get(url)
+        response = self.client.get(MOVIE_URL)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_retrieve_movie(self):
         movie = Movie.objects.create(title="Test Movie", duration=120)
-        url = reverse("cinema:movie-detail", kwargs={"pk": movie.pk})
+        url = detail_url(movie_id=movie.pk)
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
