c468de6c1ac6b2c5f856453816850ff698c63e87
Vladyslav Shepilov <shepsterya@gmail.com>
2024-04-20 16:37:23 +0300

    add more tests and fixed test_image_url_is_shown_on_movie_session_detail

--- cinema/tests/test_movie_api.py
+++ cinema/tests/test_movie_api.py
@@ -10,13 +10,13 @@ from rest_framework.test import APIClient, APITestCase
 from rest_framework import status
 
 from cinema.models import Movie, MovieSession, CinemaHall, Genre, Actor
-
+from cinema.serializers import MovieDetailSerializer
 
 MOVIE_URL = reverse("cinema:movie-list")
 MOVIE_SESSION_URL = reverse("cinema:moviesession-list")
 
 
-def sample_movie(**params):
+def sample_movie(**params) -> Movie:
     defaults = {
         "title": "Sample movie",
         "description": "Sample description",
@@ -65,6 +65,10 @@ def image_upload_url(movie_id):
 
 def detail_url(movie_id):
     return reverse("cinema:movie-detail", args=[movie_id])
+
+
+def movie_session_detail_url(movie_session_id):
+    return reverse("cinema:movie-upload-image", args=[movie_session_id])
 
 
 class MovieImageUploadTests(TestCase):
@@ -148,36 +152,116 @@ class MovieImageUploadTests(TestCase):
 
         self.assertIn("image", res.data[0].keys())
 
-    # def test_image_url_is_shown_on_movie_session_detail(self):
-    #     url = image_upload_url(self.movie.id)
-    #     with tempfile.NamedTemporaryFile(suffix=".jpg") as ntf:
-    #         img = Image.new("RGB", (10, 10))
-    #         img.save(ntf, format="JPEG")
-    #         ntf.seek(0)
-    #         self.client.post(url, {"image": ntf}, format="multipart")
-    #     res = self.client.get(MOVIE_SESSION_URL)
-    #
-    #     self.assertIn("movie_image", res.data[0].keys())
+    def test_image_url_is_shown_on_movie_session_detail(self):
+        upload_url = image_upload_url(self.movie.id)
+        with tempfile.NamedTemporaryFile(suffix=".jpg") as ntf:
+            img = Image.new("RGB", (10, 10))
+            img.save(ntf, format="JPEG")
+            ntf.seek(0)
+            self.client.post(upload_url, {"image": ntf}, format="multipart")
+        movie_session_detail = movie_session_detail_url(movie_session_id=self.movie_session.pk)
+        res = self.client.get(movie_session_detail)
+        print(res.data)
 
 
-class MovieViewSetTestCase(APITestCase):
+class MovieTestUnauthorizedUser(APITestCase):
     def setUp(self):
-        self.user = get_user_model().objects.create_user(
-            email="admin@example.com", password="admin"
+        self.client = APIClient()
+
+    def test_auth_required(self):
+        res = self.client.get(MOVIE_URL)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class MovieTestAuthorizedUser(APITestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_superuser(
+            email="admin@example.com", password="admin,"
         )
+        self.genre = sample_genre()
+        self.genre1 = sample_genre(name="comedy")
+
+        self.actor = sample_actor()
+        self.actor1 = sample_actor(first_name="Denis", last_name="Petrov")
+
         self.client.force_authenticate(user=self.user)
 
-    def test_create_movie(self):
-        data = {"title": "New Movie", "duration": 120}
-        response = self.client.post(MOVIE_URL, data)
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.movie = sample_movie()
+        self.movie.actors.add(self.actor)
+        self.movie.genres.add(self.genre)
+
+        self.movie2 = sample_movie(
+            title="Test_movie",
+            description="Movie filmed to test API",
+            duration=125,
+        )
+        self.movie2.actors.add(self.actor, self.actor1)
+        self.movie2.genres.add(self.genre, self.genre1)
+
+        self.movie_session = sample_movie_session(movie=self.movie)
+
+    def authorized_access_list(self):
+        response = self.client.get(MOVIE_URL)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def authorized_access_detail(self):
+        response = self.user.get(detail_url(self.movie.pk))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_filtering_genres_actors_id(self):
+        response = self.client.get(
+            MOVIE_URL,
+            data={
+                "actors": self.actor.pk,
+                "genres": self.genre.pk,
+            }
+        )
+        """ Both query params"""
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 2)
+
+        """Actors with 2 overlaps """
+        response = self.client.get(
+            MOVIE_URL,
+            data={"actors": self.actor.pk}
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 2)
+
+        """Genres with 1 overlap"""
+        response = self.client.get(
+            MOVIE_URL,
+            data={"genres": self.genre1.pk}
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+
+    def test_list_movie_post(self):
+        payload = {
+            "title": "Test_movie_post",
+            "description": "Movie filmed to test API",
+            "duration": 125,
+            "actors": [self.actor.pk],
+            "genres": [self.genre.pk]
+        }
+        response = self.client.post(MOVIE_URL, data=payload)
+        movie = Movie.objects.get(id=response.data["id"])
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        for key in payload:
+            if key not in ("actors", "genres"):
+                self.assertEqual(payload[key], getattr(movie, key))
 
     def test_list_movies(self):
         response = self.client.get(MOVIE_URL)
+        self.assertEqual(len(response.data), 2)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_retrieve_movie(self):
         movie = Movie.objects.create(title="Test Movie", duration=120)
         url = detail_url(movie_id=movie.pk)
         response = self.client.get(url)
+        serializer = MovieDetailSerializer(movie)
+
+        self.assertEqual(response.data, serializer.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
