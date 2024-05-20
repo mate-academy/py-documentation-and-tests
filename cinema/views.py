832aafa9dcef61c26cfd22c566cb550651293870
vital <vitaliyburkalo81@gmail.com>
2024-05-20 15:42:25 +0300

    added swagger documentation for MovieViewSet

--- cinema/views.py
+++ cinema/views.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 
 from django.db.models import F, Count
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import extend_schema, OpenApiParameter
 from rest_framework import viewsets, mixins, status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import action
@@ -127,6 +129,30 @@ class MovieViewSet(
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name="title",
+                type=OpenApiTypes.STR,
+                description="Filter by movie title (ex. ?title=Inception)"
+            ),
+            OpenApiParameter(
+                name="genres",
+                type={"type": "array", "items": {"type": "string"}},
+                description="Filter by genres (ex. ?genres=Sci-Fi, Drama)"
+            ),
+            OpenApiParameter(
+                name="actors",
+                type={"type": "array", "items": {"type": "string"}},
+                description="Filter by actors "
+                            "(ex. ?actors=Leonardo DiCaprio, Jack Nicholson)"
+            ),
+        ],
+    )
+    def list(self, request, *args, **kwargs):
+        """Get list of Movies with filters."""
+        return super().list(request, *args, **kwargs)
+
 
 class MovieSessionViewSet(viewsets.ModelViewSet):
     queryset = (
@@ -134,7 +160,8 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
         .select_related("movie", "cinema_hall")
         .annotate(
             tickets_available=(
-                F("cinema_hall__rows") * F("cinema_hall__seats_in_row")
+                F("cinema_hall__rows")
+                * F("cinema_hall__seats_in_row")
                 - Count("tickets")
             )
         )
