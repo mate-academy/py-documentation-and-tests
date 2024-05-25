5d5e9939084037d2b55cce9b48d228733fb543e9
Alexander <sasacihun@gmail.com>
2024-05-25 11:40:27 +0300

    style: Remove unused code. Test functionality manually

--- user/urls.py
+++ user/urls.py
@@ -5,13 +5,12 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView,
 )
 
-from user.views import CreateUserView, CreateTokenView, ManageUserView
+from user.views import CreateUserView, ManageUserView
 
 app_name = "user"
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
-    # path("login/", CreateTokenView.as_view(), name="login"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
