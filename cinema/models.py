2e701474fa7ee141436c02b7bf07ac8e3db33f70
Shershen Alexandra <hornet240204@gmail.com>
2024-06-03 13:02:57 +0300

    Fix flake8 errors

--- cinema/models.py
+++ cinema/models.py
@@ -63,8 +63,16 @@ class Movie(models.Model):
 
 class MovieSession(models.Model):
     show_time = models.DateTimeField()
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name="sessions")
-    cinema_hall = models.ForeignKey(CinemaHall, on_delete=models.CASCADE, related_name="sessions")
+    movie = models.ForeignKey(
+        Movie,
+        on_delete=models.CASCADE,
+        related_name="sessions"
+    )
+    cinema_hall = models.ForeignKey(
+        CinemaHall,
+        on_delete=models.CASCADE,
+        related_name="sessions"
+    )
 
     class Meta:
         ordering = ["-show_time"]
