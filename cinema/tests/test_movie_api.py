55fea173a9c6d89e2103617635f96a5d09c321ae
charmniovi <charmniovi@tutanota.com>
2024-06-03 18:37:48 +0300

    solution

--- cinema/tests/test_movie_api.py
+++ cinema/tests/test_movie_api.py
@@ -1,15 +1,15 @@
-import tempfile
 import os
+import tempfile
 
 from PIL import Image
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
-
-from rest_framework.test import APIClient
 from rest_framework import status
-from cinema.serializers import MovieListSerializer
+from rest_framework.test import APIClient
+
 from cinema.models import Movie, MovieSession, CinemaHall, Genre, Actor
+from cinema.serializers import MovieListSerializer
 
 MOVIE_URL = reverse("cinema:movie-list")
 MOVIE_SESSION_URL = reverse("cinema:moviesession-list")
