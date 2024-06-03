f3788c7a57af5db8b753cacebed992ac92b144a8
charmniovi <charmniovi@tutanota.com>
2024-06-03 18:35:50 +0300

    solution

--- cinema/tests/test_movie_api.py
+++ cinema/tests/test_movie_api.py
@@ -8,7 +8,7 @@ from django.urls import reverse
 
 from rest_framework.test import APIClient
 from rest_framework import status
-
+from cinema.serializers import MovieListSerializer
 from cinema.models import Movie, MovieSession, CinemaHall, Genre, Actor
 
 MOVIE_URL = reverse("cinema:movie-list")
@@ -157,3 +157,154 @@ class MovieImageUploadTests(TestCase):
         res = self.client.get(MOVIE_SESSION_URL)
 
         self.assertIn("movie_image", res.data[0].keys())
+
+
+class MovieViewSetUnLoggedTest(TestCase):
+
+    def test_get_list_movies(self):
+        response = self.client.get(MOVIE_URL)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class MovieViewSetUserTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        user = get_user_model().objects.create_user(
+            "admin@myproject.com", "password"
+        )
+        self.client.force_authenticate(user)
+        for _ in range(5):
+            sample_movie()
+
+    def test_get_list(self):
+        response = self.client.get(MOVIE_URL)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_list_filter_by_actor(self):
+        movie1 = sample_movie()
+        actor1 = sample_actor()
+        actor_query = f"?actors={actor1.id}"
+
+        response = self.client.get(MOVIE_URL + actor_query)
+        self.assertEqual(response.data, [])
+
+        movie1.actors.add(actor1)
+
+        response = self.client.get(MOVIE_URL + actor_query)
+        serializer = MovieListSerializer([movie1], many=True).data
+        self.assertEqual(response.data, serializer)
+
+        movie2 = sample_movie()
+        actor2 = sample_actor()
+        movie2.actors.add(actor2)
+        actor_query += f",{actor2.id}"
+
+        response = self.client.get(MOVIE_URL + actor_query)
+        serializer = MovieListSerializer([movie1, movie2], many=True).data
+        self.assertEqual(response.data, serializer)
+
+    def test_list_filter_by_genre(self):
+        movie1 = sample_movie()
+        genre1 = sample_genre()
+        genre_query = f"?genres={genre1.id}"
+
+        response = self.client.get(MOVIE_URL + genre_query)
+        self.assertEqual(response.data, [])
+
+        movie1.genres.add(genre1)
+
+        response = self.client.get(MOVIE_URL + genre_query)
+        serializer = MovieListSerializer([movie1], many=True).data
+        self.assertEqual(response.data, serializer)
+
+        movie2 = sample_movie()
+        genre2 = sample_genre(name="Comedy")
+        movie2.genres.add(genre2)
+        genre_query += f",{genre2.id}"
+
+        response = self.client.get(MOVIE_URL + genre_query)
+        serializer = MovieListSerializer([movie1, movie2], many=True).data
+        self.assertEqual(response.data, serializer)
+
+    def test_list_filter_by_title(self):
+        movie = sample_movie(title="Test")
+        title_query = f"?title={movie.title}"
+
+        response = self.client.get(MOVIE_URL + title_query + "a")
+        self.assertEqual(response.data, [])
+
+        response = self.client.get(MOVIE_URL + title_query)
+        serializer = MovieListSerializer([movie], many=True).data
+        self.assertEqual(response.data, serializer)
+
+    def test_list_filter_by_actors_genres_title(self):
+        movie = sample_movie()
+        actor = sample_actor()
+        genre = sample_genre()
+        filter_query = f"?title={movie.title}&actors={actor.id}&genres={genre.id}"
+
+        movie.actors.add(actor)
+        movie.genres.add(genre)
+
+        response = self.client.get(MOVIE_URL + filter_query)
+        serializer = MovieListSerializer([movie], many=True).data
+        self.assertEqual(response.data, serializer)
+
+    def test_unsafe_method(self):
+        response = self.client.delete(MOVIE_URL)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+        response = self.client.post(MOVIE_URL)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+        response = self.client.put(MOVIE_URL)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class MovieViewSetAdminTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        user = get_user_model().objects.create_superuser(
+            "admin@myproject.com", "password"
+        )
+        self.client.force_authenticate(user)
+
+    def test_get_detail(self):
+        movie = sample_movie()
+        response = self.client.get(
+            reverse(
+                "cinema:movie-detail",
+                kwargs={"pk": movie.id}
+            )
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_delete(self):
+        response = self.client.delete(MOVIE_URL)
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_405_METHOD_NOT_ALLOWED
+        )
+
+    def test_post(self):
+        genre = sample_genre()
+        actor = sample_actor()
+
+        data = {
+            "title": "Title",
+            "description": "Description",
+            "duration": 90,
+            "genres": [genre.id],
+            "actors": [actor.id],
+        }
+        response = self.client.post(MOVIE_URL, data)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Movie.objects.count(), 1)
+
+    def test_update(self):
+        response = self.client.patch(
+            reverse("cinema:movie-detail", kwargs={"pk": 1})
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from user.views import CreateUserView, CreateTokenView, ManageUserView
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+    TokenObtainPairView
+)
+
 
 app_name = "user"
 
@@ -7,4 +12,15 @@ urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
     path("login/", CreateTokenView.as_view(), name="login"),
     path("me/", ManageUserView.as_view(), name="manage"),
+
+    path(
+        "api/token/",
+        TokenObtainPairView.as_view(),
+        name="token_obtain_pair"
+    ),
+    path(
+        "api/token/refresh/",
+        TokenRefreshView.as_view(),
+        name="token_refresh"
+    ),
 ]
