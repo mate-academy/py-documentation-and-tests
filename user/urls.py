8219f4c584426592d9a731f046e8b48d68ecce01
Dmytro Pukas <pukasdima2002@gmail.com>
2024-06-03 12:13:37 +0200

    Flake8 fix

--- user/urls.py
+++ user/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
-    TokenVerifyView)
+    TokenVerifyView,
+)
 
 from user.views import CreateUserView, ManageUserView
 
@@ -11,7 +12,15 @@ app_name = "user"
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
     path("me/", ManageUserView.as_view(), name="manage"),
-    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path(
+        "api/token/",
+        TokenObtainPairView.as_view(),
+        name="token_obtain_pair"
+    ),
+    path(
+        "api/token/refresh/",
+        TokenRefreshView.as_view(),
+        name="token_refresh"
+    ),
     path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
 ]
