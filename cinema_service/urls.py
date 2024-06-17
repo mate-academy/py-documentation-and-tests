dee63d286084e30b295d44ae49ad91e3a02cf12c
S-SMTN <cven28@gmail.com>
2024-06-18 00:12:15 +0300

    pep8

--- cinema_service/urls.py
+++ cinema_service/urls.py
@@ -2,7 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView
+)
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
