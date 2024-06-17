ea3207c4b3975c245c9fa48f6976f0299a0c0396
S-SMTN <cven28@gmail.com>
2024-06-18 00:07:34 +0300

    implemented movie endpoints tests

--- cinema/views.py
+++ cinema/views.py
@@ -9,7 +9,7 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from rest_framework.request import Request
-from rest_framework.viewsets import GenericViewSet, ReadOnlyModelViewSet
+from rest_framework.viewsets import GenericViewSet
 
 from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession, Order
 from cinema.permissions import IsAdminOrIfAuthenticatedReadOnly
