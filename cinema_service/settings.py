2e3bc214d3eaa44ae0ec13e51da281424b4669cd
Anton Konyk <antonkonyk.com>
2024-06-27 15:22:06 +0200

    add settings for drf_spectacular working

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework.authtoken",
     "debug_toolbar",
+    "drf_spectacular",
     "cinema",
     "user",
 ]
@@ -140,3 +141,19 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Cinema Service API",
+    "DESCRIPTION": "Order tickets for movies in cinema.",
+    "VERSION": "1.0.0",
+    "SERVER_INCLUDE_SCHEMA": False,
+    "SWAGGER_UI_SETTINGS":  {
+        "deepLinking": True,
+        "defaultModelsExpanDepth": 2,
+        "defaultModelExpanDepth": 2,
+    },
+}
