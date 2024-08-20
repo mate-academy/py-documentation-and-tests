dbd5c6f5b643b2d077fa5a2462c6bef198f40476
Ostap <ostapbojko04@gmail.com>
2024-08-20 09:28:18 +0300

    add token verify and refresh

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
-from user.views import CreateUserView, CreateTokenView, ManageUserView
+from user.views import CreateTokenView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
+from user.views import CreateUserView, ManageUserView
 
 app_name = "user"
 
@@ -7,4 +13,7 @@ urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
     path("login/", CreateTokenView.as_view(), name="login"),
     path("me/", ManageUserView.as_view(), name="manage"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
