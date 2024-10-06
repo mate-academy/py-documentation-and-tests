2bafa8d72edabc1ba857b50928a85968b46dc2bd
Yaroslav Havryliuk <mordegear90@gmail.com>
2024-10-06 11:28:24 +0300

    fixed jwt token lifetime

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -165,7 +165,7 @@ SPECTACULAR_SETTINGS = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=500),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=10),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": False,
 }
