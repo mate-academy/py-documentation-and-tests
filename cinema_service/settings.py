6c0267ba28ec5756c20d5908e26fe13abf5a1281
Yakiv Lobach <lobachyakiv@gmail.com>
2024-10-08 18:01:35 +0300

    second_try

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "rest_framework.authtoken",
+    "rest_framework.authtoken"
+    "rest_framework_simplejwt",
     "debug_toolbar",
     "drf_spectacular",
     "cinema",
