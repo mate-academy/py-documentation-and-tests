486a53008d0d8d2a85cf24b3184e73edd7391b01
Olexandr Bastov <iamlucky1990@gmail.com>
2024-10-08 16:39:28 +0300

    fixes

--- cinema/views.py
+++ cinema/views.py
@@ -15,8 +15,6 @@ from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
 from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession, Order
-from cinema.permissions import IsAdminOrIfAuthenticatedReadOnly
-
 from cinema.serializers import (
     GenreSerializer,
     ActorSerializer,
@@ -40,7 +38,6 @@ class GenreViewSet(
 ):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
-    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
 
 class ActorViewSet(
@@ -50,7 +47,6 @@ class ActorViewSet(
 ):
     queryset = Actor.objects.all()
     serializer_class = ActorSerializer
-    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
 
 class CinemaHallViewSet(
@@ -60,7 +56,6 @@ class CinemaHallViewSet(
 ):
     queryset = CinemaHall.objects.all()
     serializer_class = CinemaHallSerializer
-    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
 
 class MovieViewSet(
@@ -71,7 +66,6 @@ class MovieViewSet(
 ):
     queryset = Movie.objects.prefetch_related("genres", "actors")
     serializer_class = MovieSerializer
-    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
     @staticmethod
     def _params_to_ints(qs):
@@ -137,7 +131,7 @@ class MovieViewSet(
             OpenApiExample(
                 "Example URL Request",
                 description="Example filtering by title, genres, and actors",
-                value="/api/cinema/movies/" 
+                value="/api/cinema/movies/"
                       "?title=Inception&genres=1,2&actors=3,4",
             )
         ],
@@ -183,7 +177,6 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
         )
     )
     serializer_class = MovieSessionSerializer
-    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
     def get_queryset(self):
         date = self.request.query_params.get("date")
@@ -240,7 +233,7 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
         ],
         parameters=[
             OpenApiParameter(
-                name="data",
+                name="date",
                 description="Filter by movie session date (look example)",
                 required=False,
                 type=str,
