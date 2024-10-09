e80631ab4431430ff21c74bc897b139c2b0ec2a9
serhii <ockar2018@gmail.com>
2024-10-09 13:32:20 +0300

    fix

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 from datetime import timedelta
 from pathlib import Path
-from cinema.permissions import IsAdminOrIfAuthenticatedReadOnly
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
