15de15f02a58dff2bd41f60af3a91c3671c76343
achalaidiuk <sasha.treyser90@gmail.com>
2024-10-09 13:48:57 +0300

    Fixed review changes

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -152,6 +152,9 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
+    "DEFAULT_PERMISSION_CLASSES": [
+        "cinema.permissions.IsAdminOrIfAuthenticatedReadOnly"
+    ],
 }
 
 
