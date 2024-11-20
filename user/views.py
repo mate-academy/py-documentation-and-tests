c65d544dfe0ad365eafff4bc29918e9e19d92d9b
Maksym Protsak <maksym.protsak@gmail.com>
2024-11-20 07:08:29 +0200

    Removed serializer_class from CreateTokenView, authentication_classes and permission_classes from ManageUserView.

--- user/views.py
+++ user/views.py
@@ -1,6 +1,4 @@
 from rest_framework import generics
-from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
@@ -17,7 +15,6 @@ class CreateUserView(generics.CreateAPIView):
 
 class CreateTokenView(ObtainAuthToken):
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
-    serializer_class = AuthTokenSerializer
 
     def post(self, request, *args, **kwargs):
         """Login endpoint"""
@@ -26,8 +23,6 @@ class CreateTokenView(ObtainAuthToken):
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
 
     def get_object(self):
         return self.request.user
