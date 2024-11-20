0d3095d232922a754a74a3e520878852f005d359
Maksym Protsak <112944888+MaksymProtsak@users.noreply.github.com>
2024-11-20 08:56:42 +0200

    Created sample_movie function. Renamed CINEMA_URL to MOVIE_URL. Creatd test_movie_list in AuthenticatedCinemaApiTests.

--- cinema/tests/test_movie_session_api.py
+++ cinema/tests/test_movie_session_api.py
@@ -5,8 +5,20 @@ from rest_framework.test import APIClient
 from rest_framework.reverse import reverse
 from rest_framework import status
 
+from cinema.models import Movie
+from cinema.serializers import MovieListSerializer
 
-CINEMA_URL = reverse("cinema:moviesession-list")
+MOVIE_URL = reverse("cinema:movie-list")
+
+
+def sample_movie(**params) -> Movie:
+    defaults = {
+        "title": "Groundhog Day",
+        "description": "A narcissistic, self-centered weatherman finds himself in a time loop on Groundhog Day.",
+        "duration": "101",
+    }
+    defaults.update(params)
+    return Movie.objects.create(**defaults)
 
 
 class UnauthenticatedCinemaApiTests(TestCase):
@@ -14,7 +26,7 @@ class UnauthenticatedCinemaApiTests(TestCase):
         self.client = APIClient()
 
     def test_auth_required(self):
-        res = self.client.get(CINEMA_URL)
+        res = self.client.get(MOVIE_URL)
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
@@ -27,6 +39,10 @@ class AuthenticatedCinemaApiTests(TestCase):
         )
         self.client.force_authenticate(self.user)
 
-    def test_auth_required(self):
-        res = self.client.get(CINEMA_URL)
+    def test_movie_list(self):
+        sample_movie()
+        movies = Movie.objects.all()
+        serializer = MovieListSerializer(movies, many=True)
+        res = self.client.get(MOVIE_URL)
         self.assertTrue(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
