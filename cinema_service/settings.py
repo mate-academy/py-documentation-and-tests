81f5219b975c07ea47878ab096cb25d7454d8b9a
Viesich <v.s.vesich@gmail.com>
2024-11-20 17:39:55 +0200

    install dotenv & add to requirements.txt

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -10,7 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
+
 from pathlib import Path
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
