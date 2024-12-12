a2cbaf04b2573e532e35f0e7576431802e09005f
lev2099 <lev2099@gmail.com>
2024-12-12 02:06:03 +0200

    * Add Swagger documentation for the whole project
    - filtering by title, genres, actors for movie
    - filtering by date(BE CAREFUL IF YOU PUT UNCORRECTED DATE YOU TAKE 500 code) , movie for movie session

--- cinema/views.py
+++ cinema/views.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 
+from drf_spectacular.utils import extend_schema, OpenApiParameter
+
 from django.db.models import F, Count
 from rest_framework import viewsets, mixins, status
 from rest_framework.authentication import TokenAuthentication
@@ -7,7 +9,7 @@ from rest_framework.decorators import action
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
-from rest_framework.viewsets import GenericViewSet, ReadOnlyModelViewSet
+from rest_framework.viewsets import GenericViewSet
 
 from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession, Order
 from cinema.permissions import IsAdminOrIfAuthenticatedReadOnly
@@ -127,6 +129,30 @@ class MovieViewSet(
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                "title",
+                type={"type": "string", "items": {"type": "str"}},
+                description="Filter by title name (ex. ?title=text)",
+            ),
+            OpenApiParameter(
+                "genres",
+                type={"type": "array", "items": {"type": "number"}},
+                description="Filter by genres id (ex. ?genres=1,2,3)",
+            ),
+            OpenApiParameter(
+                "actors",
+                type={"type": "array", "items": {"type": "number"}},
+                description="Filter by actors id (ex. ?actors=1,3,6)",
+            )
+        ]
+    )
+    def list(self, request, *args, **kwargs):
+        """Get list of movies
+        Can be filtering with params"""
+        return super().list(request, *args, **kwargs)
+
 
 class MovieSessionViewSet(viewsets.ModelViewSet):
     queryset = (
@@ -166,6 +192,25 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
             return MovieSessionDetailSerializer
 
         return MovieSessionSerializer
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                "date",
+                type={"type": "string", "format":"date",  "items": {"type": "str"}},
+                description="Filter by date (ex. ?date=Y-m-d) or like this (ex. ?date=2024-10-08) ",
+            ),
+            OpenApiParameter(
+                "movie",
+                type={"type": "array", "items": {"type": "number"}},
+                description="Filter by movie id (ex. ?movie=1,2,3)",
+            )
+        ]
+    )
+    def list(self, request, *args, **kwargs):
+        """Get list of movies
+        Can be filtering with params"""
+        return super().list(request, *args, **kwargs)
 
 
 class OrderPagination(PageNumberPagination):
