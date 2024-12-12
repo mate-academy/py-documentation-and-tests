0063a5cc3153c346f75cfadffe818479699963bb
lev2099 <lev2099@gmail.com>
2024-12-12 02:14:22 +0200

    * Add throttling settings
    - 10 requests per minute for unauthorized users
    - 30 requests per minute for authorized ones

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -150,6 +150,14 @@ REST_FRAMEWORK = {
         "cinema.permissions.IsAdminOrIfAuthenticatedReadOnly",
     ],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle"
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+        "anon": "10/minute",
+        "user": "30/minute"
+    }
 }
 
 SPECTACULAR_SETTINGS = {
