92a385aa00fd81492afc2386e5d108266fa7d79a
Yaroslav Bordovoy <yaroslavbordovoy@gmail.com>
2024-12-17 19:53:56 +0200

    fix some AI note's

--- cinema/tests/test_movie_api.py
+++ cinema/tests/test_movie_api.py
@@ -10,7 +10,7 @@ from rest_framework.test import APIClient
 from rest_framework import status
 
 from cinema.models import Movie, MovieSession, CinemaHall, Genre, Actor
-from cinema.serializers import MovieSerializer, MovieListSerializer, MovieDetailSerializer
+from cinema.serializers import MovieListSerializer, MovieDetailSerializer
 
 MOVIE_URL = reverse("cinema:movie-list")
 MOVIE_SESSION_URL = reverse("cinema:moviesession-list")
@@ -20,7 +20,7 @@ def detail_movie_url(id: int):
 
 
 def detail_url(movie_id):
-    return reverse("cinema:movie-detail", args=("movie_id",))
+    return reverse("cinema:movie-detail", args=(movie_id,))
 
 
 def sample_movie(**params):
