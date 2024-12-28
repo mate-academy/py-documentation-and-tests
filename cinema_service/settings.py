d6e56d758e05789e54c3b6a59377a945745bbb09
Kateryna <kat.rin47u@gmail.com>
2024-12-28 18:05:55 +0000

    chore: adjust settings.py

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -12,10 +12,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from datetime import timedelta
 from pathlib import Path
 
-import os
-from dotenv import load_dotenv
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,10 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
-load_dotenv()
-
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = (
+    "django-insecure-6vubhk2$++agnctay_4pxy_8cq)mosmn(*-#2b^v4cgsh-^!i3"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
