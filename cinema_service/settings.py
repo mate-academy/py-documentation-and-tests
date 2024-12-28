ead0bbe4e8f81e397da56fc2e0f62ba02220676c
Kateryna <kat.rin47u@gmail.com>
2024-12-28 18:02:07 +0000

    fix: replace `python-decouple` with `python-dotenv` for SECRET_KEY handling

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -13,7 +13,7 @@ from datetime import timedelta
 from pathlib import Path
 
 import os
-from decouple import config
+from dotenv import load_dotenv
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = config("SECRET_KEY")
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
