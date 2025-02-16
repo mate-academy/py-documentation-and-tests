857665780c7d40814724aeed2c998783552007b7
Trukhanov Danylo <trukhanov.danya@gmail.com>
2025-02-16 16:39:29 +0200

    Fix flake8 warnings

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+    TokenObtainPairView
+)
 
 from user.views import CreateUserView, ManageUserView
 
@@ -7,7 +10,7 @@ app_name = "user"
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("me/", ManageUserView.as_view(), name="manage"),
 ]
