904234df6049b811af7cf57b9eac5ca80dacf37d
Yura Chornyi <chorny.yura@gmail.com>
2025-03-29 16:32:01 +0200

    fix: add permission_class = AllowAny to CreateUserView

--- user/views.py
+++ user/views.py
@@ -1,11 +1,12 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 from user.serializers import UserSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
 
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
