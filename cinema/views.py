4950dfd1ef87ea7d5aede78bc6457463556ca4ac
PVS1905 <viktor66462@gmail.com>
2025-04-07 19:56:56 +0300

    Solution_new

--- cinema/views.py
+++ cinema/views.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-
 from django.db.models import F, Count
 from rest_framework import viewsets, mixins, status
 from rest_framework.decorators import action
@@ -7,7 +6,6 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
-
 from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession, Order
 from drf_spectacular.utils import (
     extend_schema,
