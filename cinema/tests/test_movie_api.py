5fcc9e57e28280b46e9b0002fcb42ab53fa13128
Alex Bul <tarasov57228@gmail.com>
2025-04-15 20:46:56 +0300

    'Solution'

--- cinema/tests/test_movie_api.py
+++ cinema/tests/test_movie_api.py
@@ -6,12 +6,13 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
-from rest_framework.test import APIClient
+from rest_framework.test import APIClient, APITestCase
 from rest_framework import status
 
 from cinema.models import Movie, MovieSession, CinemaHall, Genre, Actor
 
-MOVIE_URL = reverse("cinema:movie-list")
+
+MOVIES_URL = reverse("cinema:movie-list")
 MOVIE_SESSION_URL = reverse("cinema:moviesession-list")
 
 
@@ -103,7 +104,7 @@ class MovieImageUploadTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_post_image_to_movie_list(self):
-        url = MOVIE_URL
+        url = MOVIES_URL
         with tempfile.NamedTemporaryFile(suffix=".jpg") as ntf:
             img = Image.new("RGB", (10, 10))
             img.save(ntf, format="JPEG")
@@ -143,7 +144,7 @@ class MovieImageUploadTests(TestCase):
             img.save(ntf, format="JPEG")
             ntf.seek(0)
             self.client.post(url, {"image": ntf}, format="multipart")
-        res = self.client.get(MOVIE_URL)
+        res = self.client.get(MOVIES_URL)
 
         self.assertIn("image", res.data[0].keys())
 
@@ -157,3 +158,62 @@ class MovieImageUploadTests(TestCase):
         res = self.client.get(MOVIE_SESSION_URL)
 
         self.assertIn("movie_image", res.data[0].keys())
+
+class MovieViewSetTests(APITestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            "user@example.com", "testpass123"
+        )
+        self.client.force_authenticate(self.user)
+
+        self.genre1 = sample_genre("Action")
+        self.genre2 = sample_genre("Comedy")
+
+        self.actor1 = sample_actor("Tom", "Hanks")
+        self.actor2 = sample_actor("Will", "Smith")
+
+        self.movie1 = Movie.objects.create(
+            title="Avengers", description="Superheroes save the world", duration=143
+        )
+        self.movie1.genres.add(self.genre1)
+        self.movie1.actors.add(self.actor1)
+
+        self.movie2 = Movie.objects.create(
+            title="Bad Boys", description="Detectives in Miami", duration=120
+        )
+        self.movie2.genres.add(self.genre2)
+        self.movie2.actors.add(self.actor2)
+
+    def test_list_movies(self):
+        res = self.client.get(MOVIES_URL)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.data), 2)
+
+    def test_filter_movies_by_title(self):
+        res = self.client.get(MOVIES_URL, {"title": "Avengers"})
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data[0]["title"], "Avengers")
+        self.assertEqual(len(res.data), 1)
+
+    def test_filter_by_genres(self):
+        res = self.client.get(MOVIES_URL, {"genres": f"{self.genre1.id}"})
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data[0]["title"], self.movie1.title)
+        self.assertEqual(len(res.data), 1)
+
+    def test_filter_by_actors(self):
+        res = self.client.get(MOVIES_URL, {"actors": f"{self.actor2.id}"})
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data[0]["title"], self.movie2.title)
+        self.assertEqual(len(res.data), 1)
+
+    def test_movie_detail(self):
+        url = reverse("cinema:movie-detail", args=[self.movie1.id])
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data["title"], self.movie1.title)
+
+    def test_unauthenticated_user_cannot_access_list(self):
+        self.client.force_authenticate(user=None)
+        res = self.client.get(MOVIES_URL)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
