51413a5915d955295817df946d9757c1cc9c6629
lyaskov <lyaskov1@gmail.com>
2025-04-30 23:35:47 +0300

    Fix code style.

--- cinema_service/urls.py
+++ cinema_service/urls.py
@@ -7,8 +7,10 @@ from drf_spectacular.views import (
     SpectacularSwaggerView,
     SpectacularRedocView,
 )
-from rest_framework_simplejwt.views import (TokenObtainPairView,
-                                            TokenRefreshView)
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -16,7 +18,9 @@ urlpatterns = [
     path("api/user/", include("user.urls", namespace="user")),
     path("__debug__/", include("debug_toolbar.urls")),
     # JWT endpoints
-    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path(
+        "api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"
+    ),
     path(
         "api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"
     ),
