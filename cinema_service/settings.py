9b5b7bb83f654072d2db5e4a2e71af1ecaf5b195
RuslanTopal <baranotik15@gmail.com>
2025-05-06 01:41:43 +0000

    add base svager doc

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     "debug_toolbar",
     "cinema",
     "user",
+    "drf_spectacular"
 ]
 
 MIDDLEWARE = [
@@ -140,3 +141,7 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+   "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
