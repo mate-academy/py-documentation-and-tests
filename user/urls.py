da1871acada94aeffebe96c414d8f08b40548096
Claymore-AI <dimalishchuk2003@gmail.com>
2025-05-07 08:57:44 +0300

    fix urls

--- user/urls.py
+++ user/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
     path("login/", CreateTokenView.as_view(), name="login"),
     path("me/", ManageUserView.as_view(), name="manage"),
-    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
