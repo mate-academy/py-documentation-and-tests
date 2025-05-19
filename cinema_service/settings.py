3d78b7806e67154c72d8e3590d19b33fa98e02c2
bgbro <bgbro.inc@gmail.com>
2025-05-19 11:00:46 +0300

    completed

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework.authtoken",
     "debug_toolbar",
+    "rest_framework_simplejwt",
     "drf_spectacular",
     "cinema",
     "user",
